abb4780e866f5770d882488e0165d9bdc91382ef
Lana <lana.pavlova2005@yandex.ru>
2024-11-19 02:59:29 +0300

    Simple notification done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.mynotification"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.mynotification"
--- app/src/main/java/com/example/mynotification/MainActivity.kt
+++ app/src/main/java/com/example/mynotification/MainActivity.kt
@@ -1,20 +1,127 @@
 package com.example.mynotification
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.os.Build
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import android.Manifest
+import android.app.PendingIntent
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var manager: NotificationManagerCompat
+
+    companion object {
+        val NORMAL_CHANNEL = "NORMAL_CHANNEL"
+        val IMPORTANT_CHANNEL = "IMPORTANT_CHANNEL"
+        const val SIMPLE_NOTIFICATION_ID = 1
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        manager = NotificationManagerCompat.from(this)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name =
+                resources.getString(R.string.NOT_IMPORTANT_CHANNEL_NAME)
+            val channel = NotificationChannel(
+                NORMAL_CHANNEL, name,
+                NotificationManager.IMPORTANCE_LOW
+            )
+            val description =
+                resources.getString(R.string.NOT_IMPORTANT_CHANNEL_DESCRIPTION)
+            channel.description = description
+            channel.enableVibration(false)
+            manager.createNotificationChannel(channel)
         }
     }
+
+    fun simpleNotification(view: View) {
+        val builder = NotificationCompat.Builder(this, NORMAL_CHANNEL)
+        builder
+            .setSmallIcon(android.R.drawable.btn_star)
+            .setContentTitle("Простое оповещение")
+            .setContentText("Что-то важное произошло")
+            .setLargeIcon(
+                BitmapFactory.decodeResource(
+                    resources,
+                    android.R.drawable.btn_star_big_on
+                )
+            )
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //    public void onRequestPermissionsResult(int requestCode, String[l permissions,
+            //    int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                0
+            )
+            return
+        }
+        manager.notify(R.id.SIMPLE_NOTIFICATION_ID, builder.build())
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 0) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Разрешение получено, теперь можно отправить уведомление
+                simpleNotification(View(this))
+            } else {
+                // Разрешение не получено, обработайте это соответствующим образом
+                // Например, покажите пользователю сообщение о том, что разрешение необходимо
+            }
+        }
+    }
+
+    fun simpleCancel(view: View) {
+        manager.cancel(R.id.SIMPLE_NOTIFICATION_ID)
+    }
+
+    fun browserNotification(view: View) {
+        val a2 = Intent(this, Activity2::class.java)
+        val pa2 = PendingIntent.getActivity(
+            this, R.id.BROWSER_PENDING_ID,
+            a2, PendingIntent.FLAG_MUTABLE
+        )
+        val builder = NotificationCompat.Builder(this, NORMAL_CHANNEL)
+        builder
+            .setSmallIcon(android.R.drawable.sym_def_app_icon)
+            .setContentTitle("Запустить браузер")
+            .setContentText("Посмотреть google.com")
+            .setContentIntent(pa2)
+            .setAutoCancel(true)
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        manager.notify(R.id.GOOGLE_NOTIFICATION_ID, builder.build())
+    }
+
 }
